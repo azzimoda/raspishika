b2f679a082a052dc02a5a5cc998fe5441e08513a
Konstantin Matsa <konstantinmaca@gmail.com>
2025-05-10 12:16:04 +0500

    feat [cache]: Make cache regenerate if the value of generated cache is `nil`; Add ability to let `nil` cache be saved before expiration.

--- cache.rb
+++ cache.rb
@@ -2,12 +2,15 @@ module Cache
   @data = {}
   @mutex = Mutex.new
 
-  def self.fetch(key, expires_in: nil, &block)
+  def self.fetch(key, expires_in: nil, allow_nil: false, &block)
+    $logger.debug "Fetching cache for: #{key.inspect}"
     @mutex.synchronize do
       entry = @data[key]
-      if entry && (expires_in.nil? || Time.now - entry[:timestamp] < expires_in)
+      if entry && (allow_nil || entry[:value]) && (expires_in.nil? || Time.now - entry[:timestamp] < expires_in)
+        $logger.debug "Returning existing cache"
         entry[:value]
       else
+        $logger.debug "Generating new cache"
         @data[key] = { value: block.call, timestamp: Time.now }
         @data[key][:value]
       end
@@ -17,4 +20,6 @@ module Cache
   def self.clear
     @mutex.synchronize { @data.clear }
   end
+
+  # TODO: Maybe save cache for situations when I need to just restart bot for less than 5 minutes.
 end
