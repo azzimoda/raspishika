da294a4b8a65c4295bc1ded6ca9a9768f39fcc0d
Konstantin Matsa <konstantinmaca@gmail.com>
2025-09-24 08:34:50 +0500

    feat(logger): Add custom colored formatter.

--- src/logger.rb
+++ src/logger.rb
@@ -4,6 +4,48 @@ require 'fileutils'
 require 'logger'
 
 module Raspishika
+  class Formatter < ::Logger::Formatter
+    COLOR_CODES = {
+      black: 30, red: 31, green: 32, yellow: 33, blue: 34, magenta: 35, cyan: 36, white: 37, gray: 90, light_red: 91,
+      light_green: 92, light_yellow: 93, light_blue: 94, light_magenta: 95, light_cyan: 96, light_white: 97
+    }.freeze
+
+    RESET_CODE = "\e[0m"
+
+    def call(severity, time, progname, msg)
+      color = severity_color severity
+      msg_color = severity == 'INFO' ? nil : color
+
+      severity_shorting = colorize severity[0], color
+      formatted_time = colorize "[#{time.iso8601(6)}]", :gray
+      severity = colorize severity.rjust(5), color
+      progname = colorize progname, msg_color if msg_color
+      msg = colorize msg, msg_color if msg_color
+
+      "#{severity_shorting}, #{formatted_time} #{severity} -- #{progname}: #{msg}\n"
+    end
+
+    private
+
+    def severity_color(severity)
+      case severity
+      when 'DEBUG' then :gray # :light_blue
+      when 'INFO'  then :green
+      when 'WARN'  then :yellow
+      when 'ERROR' then :light_red
+      when 'FATAL' then :red
+      else :white
+      end
+    end
+
+    def colorize(text, color)
+      color_code = COLOR_CODES[color]
+      return text unless color_code
+
+      "\e[#{color_code}m#{text}#{Raspishika::Formatter::RESET_CODE}"
+    end
+  end
+
   class Logger
     attr_reader :log_file
 
@@ -25,6 +67,7 @@ module Raspishika
         end
 
       @stderr_logger = ::Logger.new($stderr, level: level)
+      @stderr_logger.formatter = Formatter.new
       return unless @log_file
 
       FileUtils.mkdir_p File.dirname @log_file
